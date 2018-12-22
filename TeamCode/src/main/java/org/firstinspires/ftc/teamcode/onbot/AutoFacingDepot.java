18efadbdcaa51cb799a45e329b685a0666d2aab6
Suriya <suriya.d.vijay@gmail.com>
2018-12-22 11:27:51 -0500

    First try to execute different paths based on gold mineral position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/AutoFacingDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/AutoFacingDepot.java
@@ -16,6 +16,45 @@ public class AutoFacingDepot extends BotBase{
 
         String goldLocation = getGoldLocation(5);
 
+        if(goldLocation.equals("Left")){
+            turnDegrees(-45,8);//Turn to face the gold mineral
+            runForward(36,8);
+            turnDegrees(86,8);
+            runForward(37,8);
+            dropMarker();
+            resetMarker();
+            runForward(-101,8);
+
+
+        }else if (goldLocation.equals("Right")){
+            turnDegrees(45,5);
+            runForward(47,5);
+            turnDegrees(-86,5);
+            runForward(37,5);
+            dropMarker();
+            resetMarker();
+            runForward(101,5);
+
+
+        }else if (goldLocation.equals("Center")){
+            runForward(67,11);//move back into the depot while pushing mineral
+            turnDegrees(43,8);// turn to face crater
+            dropMarker();//drop the marker
+            resetMarker();//reset the arm
+            runForward(-103,11);//move back into crater
+
+        }
+        else{
+            runForward(4, 5); // first move forward a little
+            turnDegrees(-45, 6.0); // turn left
+            runForward(44, 5); // run forward past the mineral marker
+            turnDegrees(101, 6.0); // turn right towards the depot
+            runForward(36, 5); // run into depot
+            dropMarker();
+            resetMarker();
+            runForward(-95, 6); // run towards the crater
+        }
+
         /* Used in Dayton tournament
         runForward(4, 5); // first move forward a little
         turnDegrees(-45, 6.0); // turn left
